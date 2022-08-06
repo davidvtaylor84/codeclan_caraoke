5ce03fe4e4817ba590bceb2d0447ba7f00b65d6b
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-08-06 12:42:18 +0100

    customer can pay for karaoke room

--- src/guest.py
+++ src/guest.py
@@ -3,3 +3,11 @@ class Guest:
     def __init__(self, guest_name, wallet):
         self.guest_name = guest_name
         self.wallet = wallet
+
+    def pay_for_room(self, room, guest):
+        if self.wallet >= room.room_price:
+            self.wallet -= room.room_price
+            return self.wallet
+        else:
+            return "You don't have enough money"
+
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -7,8 +7,9 @@ from src.song import Song
 class TestGuest(unittest.TestCase):
 
     def setUp(self):
-        self.guest1 = Guest("The Great Waldo", 150)
+        self.guest1 = Guest("The Great Waldo", 40)
         self.guest2 = Guest("Sandra the Voice of Reason", 60)
+        self.room1 = Room("Red Room", 4, 50.00 )
 
     # set-up tests
     def test_customer_has_name(self):
@@ -18,3 +19,11 @@ class TestGuest(unittest.TestCase):
         self.assertEqual(60, self.guest2.wallet)
 
     # function tests
+
+    def test_pay_for_room(self):
+        self.guest2.pay_for_room(self.room1, self.guest2)
+        self.assertEqual(10, self.guest2.wallet)
+
+    def test_cannot_pay_for_room(self):
+        no_money = self.guest1.pay_for_room(self.room1, self.guest1)
+        self.assertEqual("You don't have enough money", no_money)
